3453fcc844a686dd81ff5d5f9579228ca4f81f6a
trilochan88 <trilochan88@gmail.com>
2021-10-21 00:13:45 +0700

    Added CartServiceTest and test cases Failing test cases written for the method/s to be implemented in part 3.

--- src/test/java/CartServiceTest.java
+++ src/test/java/CartServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CartServiceTest {
@@ -14,23 +16,23 @@ public class CartServiceTest {
     }
     @Test
     public void selecting_item_and_able_to_add_item_and_quantity_increase_size_of_the_cart_to_1(){
-
+        assertTrue(false);
     }
     @Test
     public void selecting_item_and_able_to_add_item_add_quantity_in_the_cart_return_total_price_of_food_item(){
-
+        assertTrue(false);
     }
     @Test
     public void after_selecting_and_adding_multiple_items_to_cart_should_be_able_to_return_total_amount(){
-
+        assertTrue(false);
     }
     @Test
     public void removing_food_item_from_the_cart_should_reduce_the_cart_size_by_1(){
-
+        assertTrue(false);
     }
     @Test
     public void removing_food_item_from_the_cart_which_is_not_exist_should_throw_exception(){
-
+        assertTrue(false);
     }
 
 }
